a9093b717d5108b3ac40bd0cc9416abf966bd0ac
hemm <Thomas.Hemmerich.Extern@parcIT.de>
2025-09-11 16:24:32 +0200

    fixed version numbers and i18n

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideExceptionHandler, provideI18n} from '@parcit/wuf-widgets';
+import {provideWufTheme} from '@parcit/wuf-widgets/themes';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideHttpClient, withInterceptors, withInterceptorsFromDi} from '@angular/common/http';
+import {Level, provideRootLogger} from '@parcit/wuf-core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideWufTheme(),
+    provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRootLogger({ level: Level.DEBUG }),
+    provideI18n(),
+    provideExceptionHandler(),
+    provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
 };
